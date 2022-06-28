c621752f727a203ddcff5a5b322ea0c851f08590
Mahin <mahin.inovace@gmail.com>
2022-06-29 04:10:48 +0600

    made a token based authentication system and made the api for shop owner signup and signin

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('dotenv').config()
 const bodyParser = require('body-parser');
+const cors=require('cors')
 const app = express();
-const port = 3000;
+const port = 8000;
 const con= require('./dbConnection')
 const jwt = require('jsonwebtoken')
+const http = require("http");
 //middleware
+app.use(cors({
+    origin: '*'
+}));
 function authenticateToken(req, res , next){
     const authHeader=req.headers['authorization'];
     const token=authHeader && authHeader.split(' ')[1]
@@ -31,17 +36,19 @@ app.post('/signin', (req, res) => {
     con.query(`SELECT * FROM shop_owner where shop_owner.email = "${req.body.email}"`, function (err, result) {
         if (err) throw err;
         else{
-            if(req.body.pass===result[0].password_hash){
+            if(req.body.password===result[0].password_hash){
                 const user={
                     phone_number:result[0].phone_number,
                     user_name:result[0].user_name,
                     email:result[0].email,
                 }
                 const accessToken=jwt.sign(user,process.env.SECRET_KEY);
-                res.json({accessToken:accessToken})
+                res.json({accessToken:accessToken,user:user})
             }
-            else if(req.pass !== result[0].password_hash)
+            else if(req.password !== result[0].password_hash){
+                res.status(401);
                 res.send('Access Denied');
+            }
         }
     });
 });
@@ -56,6 +63,28 @@ app.post('/signup', (req, res) => {
         if (err) throw err;
         else{
             res.send('User Created');
+        }
+    });
+});
+app.post('/save-debt-for-customer', authenticateToken,(req, res) => {
+    const data={
+        customer_phone_number:req.body.customer_phone_number,
+        shop_owner_number:req.body.shop_owner_number,
+        description:req.body.description,
+        amount:req.body.amount
+    }
+    con.query(`Insert into customer_wise_debt set ?`,data ,function (err, result) {
+        if (err) throw err;
+        else{
+            res.send('User Created');
+        }
+    });
+});
+app.get('/get-shops-of-owner', authenticateToken,(req, res) => {
+    con.query(`Select * from shop where shop_owner_number= "${req.user.shop_owner_number}" `,function (err, result) {
+        if (err) throw err;
+        else{
+            res.send(result);
         }
     });
 });
